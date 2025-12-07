9530d3bd804b1cd90ab96a63f1a943b0c8933ca1
Jiting <jiting@jtcat.com>
2025-12-07 23:11:43 +0800

    fix led stuck

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 */
 #include "globals.h"
 #include "system/system.h"
-//#include "timer.h"
+// #include "timer.h"
 #include "connection/esb.h"
 #include "sensor/sensor.h"
 
@@ -52,11 +52,11 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 int main(void)
 {
 #if DT_NODE_HAS_PROP(ZEPHYR_USER_NODE, pwr_gpios)
-    gpio_pin_configure_dt(&gnd, GPIO_OUTPUT_ACTIVE);
-    gpio_pin_set_dt(&gnd, 0);
-    gpio_pin_configure_dt(&pwr, GPIO_OUTPUT_ACTIVE);
-    gpio_pin_set_dt(&pwr, 1);
-#endif 
+	gpio_pin_configure_dt(&gnd, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_set_dt(&gnd, 0);
+	gpio_pin_configure_dt(&pwr, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_set_dt(&pwr, 1);
+#endif
 #if IGNORE_RESET && BUTTON_EXISTS
 	bool reset_pin_reset = false;
 #else
@@ -72,21 +72,20 @@ int main(void)
 	set_led(SYS_LED_PATTERN_ON, SYS_LED_PRIORITY_BOOT); // Boot LED
 
 	uint8_t reboot_counter = reboot_counter_read();
-	bool booting_from_shutdown = !reboot_counter && (reset_pin_reset || button_read()); // 0 means from user shutdown or failed ram validation
+	bool booting_from_shutdown
+		= !reboot_counter && (reset_pin_reset || button_read()); // 0 means from user shutdown or failed ram validation
 
 	/* if button is not held after booting from shutdown, power off again
 	 * if button press is normal, continue boot
 	 * if button is held for 5 seconds, reset pairing and continue boot
 	 */
 
-	if (button_read())
-	{
-		while (button_read())
-		{
-			if (k_uptime_get() > 1000)
+	if (button_read()) {
+		while (button_read()) {
+			if (k_uptime_get() > 1000) {
 				set_led(SYS_LED_PATTERN_LONG, SYS_LED_PRIORITY_HIGHEST);
-			if (k_uptime_get() > 5000)
-			{
+			}
+			if (k_uptime_get() > 5000) {
 				LOG_INF("Pairing requested");
 				esb_reset_pair();
 				break;
@@ -94,51 +93,62 @@ int main(void)
 			k_msleep(1);
 		}
 #if USER_SHUTDOWN_ENABLED
-		if (k_uptime_get() < 50 && booting_from_shutdown) // debounce
+		if (k_uptime_get() < 50 && booting_from_shutdown) { // debounce
 			sys_request_system_off(false);
+		}
 #endif
-		if (k_uptime_get() <= 5000)
+		if (k_uptime_get() <= 5000) {
 			set_led(SYS_LED_PATTERN_ONESHOT_POWERON, SYS_LED_PRIORITY_HIGHEST);
-		else
+		} else {
 			set_led(SYS_LED_PATTERN_OFF, SYS_LED_PRIORITY_HIGHEST);
-	}
-	else if (booting_from_shutdown)
+		}
+	} else if (booting_from_shutdown) {
 		set_led(SYS_LED_PATTERN_ONESHOT_POWERON, SYS_LED_PRIORITY_BOOT);
+	}
 
 	bool docked = dock_read();
 
 	uint8_t reset_mode = -1;
 
-	if (reboot_counter == 0)
+	if (reboot_counter == 0) {
 		reboot_counter = 100;
-	else if (reboot_counter > 200)
+	} else if (reboot_counter > 200) {
 		reboot_counter = 200; // How did you get here
+	}
 	reset_mode = reboot_counter - 100;
 	if (reset_pin_reset && !docked) // Count pin resets while not docked
 	{
 		reboot_counter++;
 		reboot_counter_write(reboot_counter);
 		LOG_INF("Reset count: %u", reboot_counter);
-#if ADAFRUIT_BOOTLOADER && !(IGNORE_RESET && BUTTON_EXISTS) // Using Adafruit bootloader, skip DFU if reset button is in use
+#if ADAFRUIT_BOOTLOADER                                                                                                \
+	&& !(IGNORE_RESET && BUTTON_EXISTS)       // Using Adafruit bootloader, skip DFU if reset button is in use
 		(*dbl_reset_mem) = DFU_DBL_RESET_APP; // Skip DFU
 #endif
 		k_msleep(1000); // Wait before clearing counter and continuing
 	}
 	reboot_counter_write(100);
-	if (!reset_pin_reset && reset_mode == 0) // Only need to check once, if the button is pressed again an interrupt is triggered from before
+	if (!reset_pin_reset
+		&& reset_mode
+			   == 0) { // Only need to check once, if the button is pressed again an interrupt is triggered from before
 		reset_mode = -1; // Cancel reset_mode (shutdown)
+	}
 
 #if USER_SHUTDOWN_ENABLED
 	bool charging = chg_read();
 	bool charged = stby_read();
 	bool plugged = vin_read();
 
-	if (reset_mode == 0 && !booting_from_shutdown && !charging && !charged && !plugged) // Reset mode user shutdown, only if unplugged and undocked
+	if (reset_mode == 0 && !booting_from_shutdown && !charging && !charged
+		&& !plugged) { // Reset mode user shutdown, only if unplugged and undocked
 		sys_user_shutdown();
+	}
 #endif
 
-	if (!booting_from_shutdown) // ONESHOT_POWERON automatically sets LED off
+	if (!booting_from_shutdown) { // ONESHOT_POWERON automatically sets LED off
+		k_usleep(60);
 		set_led(SYS_LED_PATTERN_OFF, SYS_LED_PRIORITY_BOOT);
+	}
 
 	sys_reset_mode(reset_mode);
 
