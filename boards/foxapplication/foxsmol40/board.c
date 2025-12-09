98d7c7602fbd797aba46aab0910b8e322ba7a403
koro <koro@korostudio.cn>
2025-12-09 15:52:38 +0800

    add FoxSmol40

---
+++ boards/foxapplication/foxsmol40/board.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+#include <hal/nrf_gpio.h>
+#include <zephyr/devicetree.h>
+
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+
+#define VCC_GPIO_PIN DT_GPIO_PIN(ZEPHYR_USER_NODE, vcc_gpios)
+#define VCC_GPIO_PORT_NUM DT_PROP(DT_GPIO_CTLR(ZEPHYR_USER_NODE, vcc_gpios), port)
+
+static int board_foxsmol40_init(void)
+{
+	nrf_gpio_cfg(NRF_GPIO_PIN_MAP(VCC_GPIO_PORT_NUM, VCC_GPIO_PIN), NRF_GPIO_PIN_DIR_OUTPUT, NRF_GPIO_PIN_INPUT_DISCONNECT, NRF_GPIO_PIN_NOPULL, NRF_GPIO_PIN_D0H1, NRF_GPIO_PIN_NOSENSE);
+	nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(VCC_GPIO_PORT_NUM, VCC_GPIO_PIN));
+	return 0;
+}
+
+SYS_INIT(board_foxsmol40_init, PRE_KERNEL_1,
+	CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
