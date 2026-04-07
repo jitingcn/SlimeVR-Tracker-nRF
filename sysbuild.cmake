8c7cfadb2c08566d567ff5d4db6e25ce6bdd5ab1
Jiting <jiting@jtcat.com>
2026-04-07 14:40:46 +0800

    sync changes
    sync from 3949b8b1883ab727b6527c9330a69e1ef0293c92
    
    Co-authored-by: sctanf 36978460+sctanf@users.noreply.github.com

--- sysbuild.cmake
+++ sysbuild.cmake
@@ -1,39 +1,39 @@
 set(pm_static_dir ${CMAKE_CURRENT_LIST_DIR}/pm_static)
 set(soc_overlay_dir ${CMAKE_CURRENT_LIST_DIR}/socs)
 
-set(pm_static_override_candidates)
+set(pm_static_candidates)
 
+# (pm_static) + BOARD + QUALIFIERS
 if(DEFINED SB_CONFIG_BOARD_QUALIFIERS AND NOT SB_CONFIG_BOARD_QUALIFIERS STREQUAL "")
-  string(REPLACE "/" "_" pm_static_board_qualifiers ${SB_CONFIG_BOARD_QUALIFIERS})
-  list(APPEND pm_static_override_candidates
-    ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_board_qualifiers}.yml
-    ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_board_qualifiers}.yml
+  string(REPLACE "/" "_" pm_static_qualifiers ${SB_CONFIG_BOARD_QUALIFIERS})
+  list(APPEND pm_static_candidates
+    ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
+    ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
   )
 endif()
 
-list(APPEND pm_static_override_candidates
+# (pm_static) + BOARD
+list(APPEND pm_static_candidates
   ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}.yml
   ${pm_static_dir}/${SB_CONFIG_BOARD}.yml
 )
 
-foreach(pm_static_override_candidate ${pm_static_override_candidates})
-  if(EXISTS ${pm_static_override_candidate})
-    set(PM_STATIC_YML_FILE ${pm_static_override_candidate} CACHE INTERNAL "")
+# (pm_static) + SOC + "uf2" in BOARD or QUALIFIERS
+if((DEFINED SB_CONFIG_BOARD AND SB_CONFIG_BOARD MATCHES "uf2") OR (DEFINED SB_CONFIG_BOARD_QUALIFIERS AND SB_CONFIG_BOARD_QUALIFIERS MATCHES "uf2"))
+  list(APPEND pm_static_candidates
+    ${pm_static_dir}/pm_static_${SB_CONFIG_SOC}_uf2.yml
+    ${pm_static_dir}/${SB_CONFIG_SOC}_uf2.yml
+  )
+endif()
+
+#message("-- Partition manager static configuration search candidates: ${pm_static_candidates}")
+
+foreach(pm_static_candidate ${pm_static_candidates})
+  if(EXISTS ${pm_static_candidate})
+    set(PM_STATIC_YML_FILE ${pm_static_candidate} CACHE INTERNAL "")
     break()
   endif()
 endforeach()
-
-if(NOT DEFINED PM_STATIC_YML_FILE)
-  if(SB_CONFIG_BOARD STREQUAL "xiao_ble")
-    set(PM_STATIC_YML_FILE ${pm_static_dir}/nrf52840_xiao.yml CACHE INTERNAL "")
-  elseif(SB_CONFIG_BOARD MATCHES "^nrf52840dongle$")
-    set(PM_STATIC_YML_FILE ${pm_static_dir}/nrf52840dongle.yml CACHE INTERNAL "")
-  elseif(SB_CONFIG_SOC_NRF52833)
-    set(PM_STATIC_YML_FILE ${pm_static_dir}/nrf52833_uf2.yml CACHE INTERNAL "")
-  elseif(SB_CONFIG_SOC_NRF52840)
-    set(PM_STATIC_YML_FILE ${pm_static_dir}/nrf52840_uf2.yml CACHE INTERNAL "")
-  endif()
-endif()
 
 if(NOT DEFINED ${DEFAULT_IMAGE}_DTC_OVERLAY_FILE)
   set(dtc_overlay_candidates)
