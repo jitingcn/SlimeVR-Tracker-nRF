fcf5afa081c2185e688d04f491e70e0f75b59b68
Jiting <jiting@jtcat.com>
2026-04-08 15:37:58 +0800

    feat(build): add SoftDevice UF2 builds for nrf52840 boards
    Bundle the Nordic S140 SoftDevice and generate merged hex and
    UF2 artifacts when WITH_SOFTDEVICE is enabled.
    
    Update the CI matrix to publish _SD variants for supported UF2
    boards and pass the required flash offset through CMake instead
    of a dedicated prj_sd.conf file.
    
    Adjust sysbuild partition lookup to prefer sd-specific layouts
    for UF2 targets while keeping the existing board and SoC
    fallback behavior.
    
    current example:
    ```makefile
    pro-trk_sd:
    	$(BUILD_COMMAND_HEAD) --build-dir $(TRACKER_ROOT)/build_promicro_sd $(TRACKER_ROOT) $(PRISTINE_CONFIG) --board promicro_uf2/nrf52840/spi -- -DCONF_FILE=prj.conf -DWITH_SOFTDEVICE=ON -DCONFIG_FLASH_LOAD_OFFSET=0x27000 -DBOARD_ROOT=.
    ```

--- sysbuild.cmake
+++ sysbuild.cmake
@@ -1,78 +1,55 @@
 set(pm_static_dir ${CMAKE_CURRENT_LIST_DIR}/pm_static)
 set(soc_overlay_dir ${CMAKE_CURRENT_LIST_DIR}/socs)
-set(app_config_dir ${CMAKE_CURRENT_LIST_DIR})
 
-set(pm_static_candidates)
+if((DEFINED SB_CONFIG_BOARD AND SB_CONFIG_BOARD MATCHES "uf2") OR (DEFINED SB_CONFIG_BOARD_QUALIFIERS AND SB_CONFIG_BOARD_QUALIFIERS MATCHES "uf2"))
+  set(pm_static_candidates)
 
-if(DEFINED FILE_SUFFIX AND NOT FILE_SUFFIX STREQUAL "")
-  # (pm_static) + BOARD + QUALIFIERS
+  if(WITH_SOFTDEVICE)
+    if(DEFINED SB_CONFIG_BOARD_QUALIFIERS AND NOT SB_CONFIG_BOARD_QUALIFIERS STREQUAL "")
+      string(REPLACE "/" "_" pm_static_qualifiers ${SB_CONFIG_BOARD_QUALIFIERS})
+      list(APPEND pm_static_candidates
+        ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_qualifiers}_sd.yml
+        ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_qualifiers}_sd.yml
+      )
+    endif()
+
+    list(APPEND pm_static_candidates
+      ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_sd.yml
+      ${pm_static_dir}/${SB_CONFIG_BOARD}_sd.yml
+    )
+
+    list(APPEND pm_static_candidates
+      ${pm_static_dir}/${SB_CONFIG_SOC}_uf2_sd.yml
+      ${pm_static_dir}/pm_static_${SB_CONFIG_SOC}_uf2_sd.yml
+    )
+  endif()
+
   if(DEFINED SB_CONFIG_BOARD_QUALIFIERS AND NOT SB_CONFIG_BOARD_QUALIFIERS STREQUAL "")
     string(REPLACE "/" "_" pm_static_qualifiers ${SB_CONFIG_BOARD_QUALIFIERS})
     list(APPEND pm_static_candidates
-      ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_qualifiers}_${FILE_SUFFIX}.yml
-      ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_qualifiers}_${FILE_SUFFIX}.yml
+      ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
+      ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
     )
   endif()
 
-  # (pm_static) + BOARD
   list(APPEND pm_static_candidates
-    ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${FILE_SUFFIX}.yml
-    ${pm_static_dir}/${SB_CONFIG_BOARD}_${FILE_SUFFIX}.yml
+    ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}.yml
+    ${pm_static_dir}/${SB_CONFIG_BOARD}.yml
   )
 
-  # (pm_static) + SOC + "uf2" in BOARD or QUALIFIERS
-  if((DEFINED SB_CONFIG_BOARD AND SB_CONFIG_BOARD MATCHES "uf2") OR (DEFINED SB_CONFIG_BOARD_QUALIFIERS AND SB_CONFIG_BOARD_QUALIFIERS MATCHES "uf2"))
-    list(APPEND pm_static_candidates
-      ${pm_static_dir}/pm_static_${SB_CONFIG_SOC}_uf2_${FILE_SUFFIX}.yml
-      ${pm_static_dir}/${SB_CONFIG_SOC}_uf2_${FILE_SUFFIX}.yml
-    )
-  endif()
-endif()
-
-# (pm_static) + BOARD + QUALIFIERS
-if(DEFINED SB_CONFIG_BOARD_QUALIFIERS AND NOT SB_CONFIG_BOARD_QUALIFIERS STREQUAL "")
-  string(REPLACE "/" "_" pm_static_qualifiers ${SB_CONFIG_BOARD_QUALIFIERS})
   list(APPEND pm_static_candidates
-    ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
-    ${pm_static_dir}/${SB_CONFIG_BOARD}_${pm_static_qualifiers}.yml
-  )
-endif()
-
-# (pm_static) + BOARD
-list(APPEND pm_static_candidates
-  ${pm_static_dir}/pm_static_${SB_CONFIG_BOARD}.yml
-  ${pm_static_dir}/${SB_CONFIG_BOARD}.yml
-)
-
-# (pm_static) + SOC + "uf2" in BOARD or QUALIFIERS
-if((DEFINED SB_CONFIG_BOARD AND SB_CONFIG_BOARD MATCHES "uf2") OR (DEFINED SB_CONFIG_BOARD_QUALIFIERS AND SB_CONFIG_BOARD_QUALIFIERS MATCHES "uf2"))
-  list(APPEND pm_static_candidates
-    ${pm_static_dir}/pm_static_${SB_CONFIG_SOC}_uf2.yml
     ${pm_static_dir}/${SB_CONFIG_SOC}_uf2.yml
+    ${pm_static_dir}/pm_static_${SB_CONFIG_SOC}_uf2.yml
   )
-endif()
 
-list(REMOVE_DUPLICATES pm_static_candidates)
+  list(REMOVE_DUPLICATES pm_static_candidates)
 
-#message("-- Partition manager static configuration search candidates: ${pm_static_candidates}")
-
-foreach(pm_static_candidate ${pm_static_candidates})
-  if(EXISTS ${pm_static_candidate})
-    set(PM_STATIC_YML_FILE ${pm_static_candidate} CACHE INTERNAL "")
-    break()
-  endif()
-endforeach()
-
-if(DEFINED FILE_SUFFIX AND NOT FILE_SUFFIX STREQUAL "")
-  set(app_extra_conf_candidate ${app_config_dir}/prj_${FILE_SUFFIX}.conf)
-
-  if(EXISTS ${app_extra_conf_candidate})
-    list(APPEND ${DEFAULT_IMAGE}_EXTRA_CONF_FILE ${app_extra_conf_candidate})
-    list(REMOVE_DUPLICATES ${DEFAULT_IMAGE}_EXTRA_CONF_FILE)
-    set(${DEFAULT_IMAGE}_EXTRA_CONF_FILE
-        ${${DEFAULT_IMAGE}_EXTRA_CONF_FILE}
-        CACHE INTERNAL "")
-  endif()
+  foreach(pm_static_candidate ${pm_static_candidates})
+    if(EXISTS ${pm_static_candidate})
+      set(PM_STATIC_YML_FILE ${pm_static_candidate} CACHE INTERNAL "")
+      break()
+    endif()
+  endforeach()
 endif()
 
 set(extra_dtc_overlay_candidates)
