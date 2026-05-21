538daeb70773ab7f2399ceaea82763b4adde6604
Jiting <jiting@jtcat.com>
2026-05-21 20:34:15 +0800

    fix(sensor): preserve protocol IDs for ICM-42686
    Follow-up to bd3a7689.
    
    - keep existing SlimeVR IMU type values and report ICM-42686 as ICM-42688 until the server adds a dedicated enum
    
    - fix the WHO_AM_I scan table count for register 0x75
    
    - remove the temporary FIFO axis remap from the ICM-42686 driver

--- src/build_defines.h
+++ src/build_defines.h
@@ -62,15 +62,14 @@ static uint8_t get_server_constant_tracker_status(int status) __attribute__((unu
 #define SVR_IMU_BMI160 8
 #define SVR_IMU_ICM20948 9
 #define SVR_IMU_ICM42688 10
-#define SVR_IMU_ICM42686 11
-#define SVR_IMU_BMI270 12
-#define SVR_IMU_LSM6DS3TRC 13
-#define SVR_IMU_LSM6DSV 14
-#define SVR_IMU_LSM6DSO 15
-#define SVR_IMU_LSM6DSR 16
-#define SVR_IMU_ICM45686 17
-#define SVR_IMU_ICM45605 18
-#define SVR_IMU_ADC_RESISTANCE 19
+#define SVR_IMU_BMI270 11
+#define SVR_IMU_LSM6DS3TRC 12
+#define SVR_IMU_LSM6DSV 13
+#define SVR_IMU_LSM6DSO 14
+#define SVR_IMU_LSM6DSR 15
+#define SVR_IMU_ICM45686 16
+#define SVR_IMU_ICM45605 17
+#define SVR_IMU_ADC_RESISTANCE 18
 #define SVR_IMU_DEV_RESERVED 250
 
 #define SVR_BOARD_UNKNOWN 0
@@ -196,7 +195,9 @@ static uint8_t get_server_constant_imu_id(int id)
 	case IMU_ICM42688:
 		return SVR_IMU_ICM42688;
 	case IMU_ICM42686:
-		return SVR_IMU_ICM42686;	
+		// currently just report as 42688 since it's close enough and doesn't have a server constant yet
+		// todo: add a proper constant for it in the server and update this
+		return SVR_IMU_ICM42688;
 	case IMU_ICM45686:
 		return SVR_IMU_ICM45686;
 	case IMU_ICM45688:
