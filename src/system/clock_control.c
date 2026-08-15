1c3b380da18c62a33661b5ad3006eacf1524edd7
Spiro <spiro@spiro.ooo>
2026-08-15 18:02:11 +0100

    Add miro uf2 to build

--- src/system/clock_control.c
+++ src/system/clock_control.c
@@ -143,7 +143,7 @@ void clock_init_external(void)
 {
 #if defined(NRF_CLOCK_USE_EXTERNAL_LFCLK_SOURCES) || defined(__NRFX_DOXYGEN__)
 	if (IS_ENABLED(CONFIG_CLOCK_USE_LFXO)) {
-		clock_switch(NRF_CLOCK_LFCLK_XTAL);
+		clock_switch(NRF_CLOCK_LFCLK_XTAL_FULL_SWING);
 	} else if (IS_ENABLED(CONFIG_CLOCK_USE_LF_SYNTH)) {
 		/* Use LF synthesizer (derived from HFXO) for TDMA timing precision
 		 * when LFXO is not available on the board */
